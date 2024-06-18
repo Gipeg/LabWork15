1f663ef2f4f86e5b164957d69ee2636d23829a0e
fisp <arenatkanovna@gmail.com>
2024-06-19 00:06:54 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+
+        public delegate int MathDelegate(int x);
+
+        public static int FactorialCounter(int y)
+        {
+            int result = 1;
+            if (y < 0)
+            {
+                Console.WriteLine("Значение не верно записано");
+            }
+            else if (y == 0)
+            {
+                return 1;
+            }
+            else
+                for (int i = 1; i < y + 1; i++)
+                {
+                    result *= i;
+                }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            MathDelegate xSquare = (x) => x * x;
+            MathDelegate xFactorial = (x) => FactorialCounter(x);
+            MathDelegate xABS = (x) => Math.Abs(x);
+
+            Console.WriteLine($"Квадрат числа: {xSquare(-3)}");
+            Console.WriteLine($"Факториал: {xFactorial(-3)}");
+            Console.WriteLine($"Модуль: {xABS(3)}");
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DelegateExample
+{
+    public delegate void MathOperation(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathDelegate = new MathOperation(Sum); //сумма
+            mathDelegate += Difference; //разность
+            mathDelegate += Product; //произведение
+            mathDelegate += Quotient; // частное
+
+            int a = 10;
+                int b = 5;
+
+            mathDelegate(a, b);
+
+            static void Sum(int x, int y)
+            {
+                Console.WriteLine($"Сумма: { x + y }");
+            }
+            static void Difference(int x, int y) 
+            {
+                Console.WriteLine($"Равность: { x - y}");
+            }
+            static void Product(int x, int y)
+            {
+                Console.WriteLine($"Произведение: {x * y}");
+            }
+            static void Quotient(int x, int y)
+            {
+                if (y != 0 )
+                {
+                    Console.WriteLine($"Частное: {x / y}");
+                }
+                else
+                {
+                    Console.WriteLine("Невозможно найти");
+                }
+            }
+
+        }
+    }
+}
